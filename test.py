5ee348a2a49dc44512390bfc935300e228b359a4
natu900 <yingminanshang@gmail.com>
2024-08-31 20:07:48 +0900

    うごかない

--- test.py
+++ test.py
@@ -1,32 +1,34 @@
-# 時間を計るライブラリをインポート
-import time
-# WebDriverライブラリをインポート
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-# ChromeのWebDriverライブラリをインポート
 import chromedriver_binary
+import time
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-# Chromeを指定する
-driver = webdriver.Chrome()
-# Chromeを開いてGoogleにアクセスする
-driver.get('https://www.google.com/')
+# options = webdriver.ChromeOptions()
+# options.add_argument('--ignore-certificate-errors')
+# options.add_argument('--ignore-ssl-errors')
+# driver = webdriver.Chrome(chrome_options=options)
 
-# 5秒待機する
-time.sleep(5)
+options = Options()
+options.add_argument('--ignore-certificate-errors')  
+capabilities = DesiredCapabilities.CHROME.copy()
+capabilities['acceptInsecureCerts'] = True
 
-# ページ内でname属性がqの要素を指定する（検索ボックス）
+driver = webdriver.Chrome(options=options,desired_capabilities=capabilities)
+driver.get('https://www.bing.com/?cc=jp')
+
 search_box = driver.find_element(By.NAME,'q')
-# 指定した要素（検索ボックス）にChromeDriverと入力する
-search_box.send_keys('ChromeDriver')
-# フォームを送信する（検索する）
+search_box.send_keys(1)
 search_box.submit()
+#time.sleep(3)
+search_box.send_keys(1000)
+search_box.submit()
+#time.sleep(3)
 
-# 5秒待機する
-time.sleep(5)
+# for i in range(5):
+#     search_box.send_keys(i)
+#     search_box.submit()
+#     #time.sleep(3)
 
-# Webページのスクリーンショットを保存する（保存先はカレントディレクトリ）
-driver.save_screenshot('selenium_schreenshot_test.png')
-
-# 画面を閉じる
 driver.quit()
